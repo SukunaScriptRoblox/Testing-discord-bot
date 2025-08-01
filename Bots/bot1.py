6efb70335cb0634fdc7d73f1e7ace603cac07084
SukunaScriptRoblox <lilabenrathva1989@gmail.com>
2025-08-01 11:02:35 +0530

    Update bot1.py

--- Bots/bot1.py
+++ Bots/bot1.py
@@ -3,7 +3,7 @@ from threading import Thread
 import discord  
 from discord.ext import commands  
 import asyncio  
-import os  
+import os  from keep_alive import keep_alive
 
 # ✅ Flask keep_alive setup
 app = Flask('')
